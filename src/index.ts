6ebb4d722a3e8d937af05ad39d0680f586e87d94
Eugene <yarastqt@gmail.com>
2021-07-11 19:40:20 +0300

    feat: impl new public api

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,11 @@
-import StyleDictionary from './vendors/style-dictionary'
+import { createCompiler } from './compiler'
+import { createContext } from './context'
+import { registerBuiltins } from './builtins'
 
-import { Preset } from './index.h'
+const context = createContext()
+registerBuiltins(context)
 
-export const Api = {
-  registerFormat: StyleDictionary.registerFormat.bind(StyleDictionary),
-  registerTransform: StyleDictionary.registerTransform.bind(StyleDictionary),
-  registerAction: StyleDictionary.registerAction.bind(StyleDictionary),
-  registerFilter: StyleDictionary.registerFilter.bind(StyleDictionary),
-
-  /**
-   * Map with registered presets
-   *
-   * @internal
-   */
-  presets: new Map<string, Preset>(),
-
-  /**
-   * Add new or override existing preset
-   *
-   * @param preset - Preset settings
-   */
-  registerPreset: (preset: Preset): void => {
-    Api.presets.set(preset.name, preset)
-  },
-}
-
-/**
- * @internal
- */
-export const InternalApi = {
-  extend: StyleDictionary.extend.bind(StyleDictionary),
-}
+export const compile = createCompiler(context)
+export const registerFilter = context.registerFilter
+export const registerFormat = context.registerFormat
+export const registerTransform = context.registerTransform
